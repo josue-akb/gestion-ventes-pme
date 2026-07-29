279ca191625a9c556fde420e5668c23d57a315ea
Josue Akoubet <Akoubetandre@gmail.com.com>
2026-07-29 16:18:21 +0200

    fix (app.js) : ajout routes clients

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,11 +5,9 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
-
-
+import clientRoutes from './routes/clientRoutes.js';
 
 const app = express();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,11 +16,11 @@ app.use(helmet());
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(morgan('dev'));
 
-
 app.get('/', (req, res) => {
   res.json({ message: 'API Gestion Ventes — OK' });
 });
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/clients', clientRoutes);
 
 export default app;
