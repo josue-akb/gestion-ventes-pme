532a27a849fa14eea16fa53d754b36f1421483e5
Josue Akoubet <akoubetandre@gmail.com>
2026-09-12 13:54:34 +0200

    fix(cors): ajout URL Vercel production

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
-app.use(cors({ origin: ['http://localhost:5173','https://gestion-ventes-pme.vercel.app',],credentials: true,}));
+app.use(cors({ origin: ['http://localhost:5173','https://gestion-ventes-4149vbu5y-josues-projects-2c380559.vercel.app',],credentials: true,}));
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
